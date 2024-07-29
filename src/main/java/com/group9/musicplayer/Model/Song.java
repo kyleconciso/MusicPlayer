4d8442a244865c48655634d62e10826ee5e26b95
kyleconciso <kyleconciso@gmail.com>
2024-07-29 21:42:25 +0800

    Added song model

--- src/main/java/com/group9/musicplayer/Model/Song.java
+++ src/main/java/com/group9/musicplayer/Model/Song.java
@@ -11,5 +11,77 @@ package com.group9.musicplayer.Model;
 public class Song {
     private String title;
     private String artist;
+    private String album;
+    private int year;
+    private String genre;
+    private String filePath;
+    private String coverFilePath;
+
+    public Song(String title, String artist, String album, int year, String genre, String filePath, String coverFilePath) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.year = year;
+        this.genre = genre;
+        this.filePath = filePath;
+        this.coverFilePath = coverFilePath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getCoverFilePath() {
+        return coverFilePath;
+    }
+
+    public void setCoverFilePath(String coverFilePath) {
+        this.coverFilePath = coverFilePath;
+    }
+    
     
 }
