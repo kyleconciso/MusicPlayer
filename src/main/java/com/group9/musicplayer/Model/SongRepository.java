10acc687c00a40ba2af33088efc0b319509038c1
kyleconciso <kyleconciso@gmail.com>
2024-08-01 22:43:33 +0800

    implementation of SongRepository

--- src/main/java/com/group9/musicplayer/Model/SongRepository.java
+++ src/main/java/com/group9/musicplayer/Model/SongRepository.java
@@ -4,10 +4,54 @@
  */
 package com.group9.musicplayer.Model;
 
+import com.group9.musicplayer.Model.*;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author psalm
  */
 public class SongRepository {
-    
+    private EntityManagerFactory emf = null;
+    private EntityManager em = null;
+
+    public SongRepository() {
+       emf = Persistence.createEntityManagerFactory("employees-db");
+        em = emf.createEntityManager(); // Name from persistence.xml
+    }
+
+    public void persistEmployee(Song e) {
+        em.getTransaction().begin(); // Start transaction
+        em.persist(e);
+        em.getTransaction().commit(); // Commit changes
+    }
+
+    public void mergeEmployee(Song e) {
+        em.getTransaction().begin(); // Start transaction
+        em.merge(e);
+        em.getTransaction().commit(); // Commit changes
+    }
+
+    public void removeEmployee(Song e) {
+        em.getTransaction().begin(); // Start transaction
+        em.remove(e);
+        em.getTransaction().commit(); // Commit changes
+    }
+
+    public Song find(int id) {
+        return em.find(Song.class, id);
+    }
+
+    public List<Song> list() {
+        return em.createQuery("FROM model.Employee m", Song.class).getResultList();
+    }
+
+    public void close() {
+        emf.close();
+        em.close();
+    }
+
 }
